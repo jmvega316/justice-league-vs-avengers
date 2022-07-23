6277c60ca1ac05ff33a2e2f07b09340b45441e02
Muhammad Humayun <humayun@MacBook-Pro.home>
2022-07-23 18:58:59 -0400

    created and conntected the route to the database

---
+++ server.js
@@ -0,0 +1,23 @@
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  
+});
