fef2c5234b21bd934b522fe208e05a897514ee51
Andrew Lee <alee68@fordham.edu>
2022-07-26 00:49:39 -0400

    created post route to update the user's post by id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
+const { update } = require("../../models/PostModel");
 const withAuth = require("../../utils/auth");
 
 // posts can be created, updated, or deleted
@@ -20,4 +21,22 @@ router.post("/", withAuth, async (req, res) => {
 
 // route to update the user's post
 
+router.put("/:id", withAuth, async (res, req) => {
+	try {
+		const updatePost = await Post.update(req.body, {
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		// in case the post cannot be found
+		if (!updatePost) {
+			res.status(404).json({ message: "Cannot find the post." });
+		}
+
+		res.status(200).json(updatePost);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 // route to delete the user's post
