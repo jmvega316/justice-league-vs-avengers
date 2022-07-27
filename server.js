980bd48cb9d1e69aa0f38bece1469940f1e760f8
Danqi Yin <dy2395@columbia.edu>
2022-07-26 22:20:27 -0400

    test commit

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "/public/images")));
 
 // turn on routes
 app.use(routes);
