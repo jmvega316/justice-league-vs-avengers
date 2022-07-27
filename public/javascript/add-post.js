a5368201a903f5a624912467391b91097c8dffa4
dy2395 <71238779+dy2395@users.noreply.github.com>
2022-07-27 01:01:34 -0400

    add hero template in newpost page

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,13 +2,13 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="post-url"]').value;
+  const post_content = document.querySelector('input[name="post-content"]').value;
 
   const response = await fetch(`/api/posts`, {
-    method: 'POST',
+    method: 'post',
     body: JSON.stringify({
       title,
-      post_url
+      post_content
     }),
     headers: {
       'Content-Type': 'application/json'
@@ -18,7 +18,7 @@ async function newFormHandler(event) {
   if (response.ok) {
     document.location.replace('/dashboard');
   } else {
-    alert(response.statusText);
+    alert("Failed to create a post. Please try again.");
   }
 }
 
